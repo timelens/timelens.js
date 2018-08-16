7a1f8a2425a513c6b81620b578a1a29c060acb6e
Sebastian Morr <sebastian@morr.cc>
2018-08-16 16:28:16 +0200

    Don't require jQuery, use native JavaScript functions

--- timelens.js
+++ timelens.js
@@ -1,10 +1,105 @@
-// Make `timelens` available as a jQuery object method.
-(function($) {
-    $.fn.timelens = function(options) {
-        initTimelens(this, options);
-        return this;
+function timelens(container, options) {
+    // Load VTT file asynchronously, then continue with the initialization.
+    let vtt_url;
+    if (options.thumbnails) {
+        vtt_url = options.thumbnails;
+    }
+
+    var request = new XMLHttpRequest();
+    request.open('GET', vtt_url, true);
+    request.send(null);
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                initTimelens2(container, request.responseText, options);
+            }
+        }
+    }
+}
+
+// Actually initialize Timelens.
+function initTimelens2(container, vtt, options) {
+    var thumbnails = parseVTT(vtt);
+    var duration = thumbnails[thumbnails.length - 1].to;
+
+    // Use querySelector if a selector string is specified.
+    if (typeof container == "string")
+        container = document.querySelector(container);
+
+    // This will be our main .timelens div, which will contain all new elements.
+    if (container.className != "") {
+        container.className += " ";
+    }
+    container.className += "timelens";
+
+    // Create .thumbnail div, which contains the preview thumbnails.
+    var thumbnail = document.createElement("div");
+    thumbnail.className = "thumbnail";
+
+    // Create .timeline img, which displays the visual timeline.
+    var timeline = document.createElement("img");
+    timeline.src = options.timeline;
+    // Prevent the timeline image to be dragged
+    timeline.setAttribute("draggable", "false");
+
+    // Assemble everything together.
+    container.appendChild(timeline);
+    container.appendChild(thumbnail);
+
+    // Create .marker div, which is used to display the current position.
+    if (options.position) {
+        var marker = document.createElement("div");
+        marker.className = "marker";
+        container.appendChild(marker);
+    }
+
+    // When clicking the timeline, seek to the respective position.
+    if (!!options.seek) {
+        timeline.onclick = function(event) {
+            var progress = progressAtMouse(event, timeline);
+            options.seek(progress * duration);
+        };
+    }
+
+    timeline.onmousemove = function(event) {
+        // Calculate click position in seconds.
+        var progress = progressAtMouse(event, timeline);
+        let seconds = progress * duration;
+
+        let thumbnail_dir = options.thumbnails.substring(0, options.thumbnails.lastIndexOf("/") + 1);
+
+        // Find the first entry in `thumbnails` which contains the current position.
+        let active_thumbnail = null;
+        for (let t of thumbnails) {
+            if (seconds >= t.from && seconds <= t.to) {
+                active_thumbnail = t;
+                break
+            }
+        }
+
+        // Set respective background image.
+        thumbnail.style["background-image"] = "url(" + thumbnail_dir + active_thumbnail.file + "), url(loading.png)";
+        // Move background to the correct location.
+        thumbnail.style["background-position"] = -active_thumbnail.x + "px " + -active_thumbnail.y + "px";
+
+        // Set thumbnail div to correct size.
+        thumbnail.style.width = active_thumbnail.w + "px";
+        thumbnail.style.height = active_thumbnail.h + "px";
+
+        // Move thumbnail div to the correct position.
+        thumbnail.style.marginLeft = Math.min(
+            Math.max(0, x - active_thumbnail.w / 2 - 5),
+            timeline.offsetWidth - active_thumbnail.w
+        ) + "px";
     };
-}(jQuery));
+
+    if (options.position) {
+        setInterval(function() {
+            marker.style.marginLeft = options.position() / duration * timeline.offsetWidth - 11 + "px";
+        }, 1);
+    }
+}
 
 // Convert a WebVTT timestamp (which has the format [HH:]MM:SS.mmm) to seconds.
 function from_timestamp(timestamp) {
@@ -22,116 +117,7 @@ function from_timestamp(timestamp) {
 // How far is the mouse into the timeline, in a range from 0 to 1?
 function progressAtMouse(event, timeline) {
     x = event.offsetX ? event.offsetX : event.pageX - timeline.offsetLeft;
-    return x / timeline.width();
-}
-
-// Load VTT file asynchronously, then continue with the initialization.
-function initTimelens(element, options) {
-    let vtt_url;
-    if (options.thumbnails) {
-        vtt_url = options.thumbnails;
-    }
-
-    var request = new XMLHttpRequest();
-    request.open('GET', vtt_url, true);
-    request.send(null);
-    request.onreadystatechange = function() {
-        if (request.readyState === 4 && request.status === 200) {
-            var type = request.getResponseHeader('Content-Type');
-            if (type.indexOf("text") !== 1) {
-                initTimelens2(element, request.responseText, options);
-            }
-        }
-    }
-}
-
-// Actually initialize Timelens.
-function initTimelens2(element, vtt, options) {
-    var thumbnails = parseVTT(vtt);
-    var duration = thumbnails[thumbnails.length - 1].to;
-
-    // This will be our main .timelens div, which will contain all new elements.
-    var timelens = element;
-    timelens.addClass("timelens");
-
-    // Create .thumbnail div, which contains the preview thumbnails.
-    var thumbnail = $(document.createElement("div"));
-    thumbnail.attr("class", "thumbnail");
-
-    // Create .timeline img, which displays the visual timeline.
-    var timeline = $(document.createElement("img"));
-    timeline.attr("src", options.timeline);
-    // Prevent the timeline image to be dragged
-    timeline.attr("draggable", "false");
-
-    // Assemble everything together.
-    element.append(timelens.get(0));
-    timelens.append(thumbnail.get(0));
-    timelens.append(timeline.get(0));
-
-    // Create .marker div, which is used to display the current position.
-    if (options.position) {
-        var marker = $(document.createElement("div"));
-        marker.attr("class", "marker");
-        timelens.append(marker.get(0));
-    }
-
-    // When clicking the timeline, seek to the respective position.
-    if (!!options.seek) {
-        timeline.click(function(event) {
-            var progress = progressAtMouse(event, timeline);
-            options.seek(progress * duration);
-        });
-    }
-
-    // Fade thumbnail in/out when mouse enters/leaves the timeline.
-    timeline.mouseover(function(event) {
-        thumbnail.fadeIn(100);
-    });
-    timeline.mouseout(function(event) {
-        thumbnail.fadeOut(100);
-    });
-
-    timeline.mousemove(function(event) {
-        // Calculate click position in seconds.
-        var progress = progressAtMouse(event, timeline);
-        let seconds = progress * duration;
-
-        let thumbnail_dir = options.thumbnails.substring(0, options.thumbnails.lastIndexOf("/") + 1);
-
-        // Find the first entry in `thumbnails` which contains the current position.
-        let active_thumbnail = null;
-        for (let t of thumbnails) {
-            if (seconds >= t.from && seconds <= t.to) {
-                active_thumbnail = t;
-                break
-            }
-        }
-
-        // Set respective background image.
-        thumbnail.css(
-            "background-image",
-            "url(" + thumbnail_dir + active_thumbnail.file + "), url(loading.png)"
-        );
-        // Move background to the correct location.
-        thumbnail.css("background-position", -active_thumbnail.x + "px " + -active_thumbnail.y + "px");
-
-        // Set thumbnail div to correct size.
-        thumbnail.css("width", active_thumbnail.w);
-        thumbnail.css("height", active_thumbnail.h);
-
-        // Move thumbnail div to the correct position.
-        thumbnail.get(0).style.marginLeft = Math.min(
-            Math.max(0, x - active_thumbnail.w / 2 - 5),
-            timeline.width() - active_thumbnail.w
-        ) + "px";
-    });
-
-    if (options.position) {
-        setInterval(function() {
-            marker.get(0).style.marginLeft = options.position() / duration * timeline.width() - 11 + "px";
-        }, 1);
-    }
+    return x / timeline.offsetWidth;
 }
 
 // Parse a VTT file pointing to JPEG files using media fragment notation.
